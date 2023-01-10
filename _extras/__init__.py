818d7fb8775b088a339fa39783c8336018c47327
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-01-10 16:17:33 +0530

    Update __init__.py

--- _extras/__init__.py
+++ _extras/__init__.py
@@ -1,3 +1,4 @@
 import os
 
-SUDOS = list(map(int, os.environ.get("SUDOS", "5304356242 5370531116 5551387300")))
+SUDOS = os.environ.get("SUDOS", "5304356242 5370531116 5551387300")
+SUDOS = list(map(int, SUDOS.split()))

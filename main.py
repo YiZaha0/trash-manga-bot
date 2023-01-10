76f3c66ed8273b411c32c174d68d20978458e25b
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-01-10 15:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+import importlib
+import sys 
 import asyncio as aio
+import glob
 import os
 
 from bot import bot, manga_updater
 from models import DB
+from pathlib import Path
 
+def load_plugin(plugin_name):
+    if plugin_name.startswith("__"):
+    	pass
+    else:
+    	path = Path(f"_extras/{plugin_name}.py")
+    	name = "_extras.{}".format(plugin_name)
+    	spec = importlib.util.spec_from_file_location(name, path)
+    	load = importlib.util.module_from_spec(spec)
+    	load.logger = logging.getLogger(plugin_name)
+    	spec.loader.exec_module(load)
+    	sys.modules["_extras." + plugin_name] = load
+
+path = "./_extras/*py"
+files = glob.glob(path)
+for name in files:
+	plugin_path = Path(name)
+	plugin_name = plugin_path.stem
+	try:
+		load_plugin(plugin_name)
+	except BaseException as e:
+		print(f"Got Error While Loading {plugin_name}: {e}")
 
 async def async_main():
     db = DB()

a7177c7cfba0a558c597f76073473cf45c5a16fa
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-01-17 01:25:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,7 +129,8 @@ SUDOS = list(map(int, SUDOS.split()))
 bot = Client('bot',
              api_id=int(env_vars.get('API_ID')),
              api_hash=env_vars.get('API_HASH'),
-             bot_token=env_vars.get('BOT_TOKEN'),)
+             bot_token=env_vars.get('BOT_TOKEN'),
+             ipv6=bool(env_vars.get('IPV6_CLIENT')))
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL')
 if dbname:

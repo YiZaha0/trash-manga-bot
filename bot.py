96d5802d4c255e7419ffb344713538a90549a624
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-01-18 00:07:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -558,7 +558,7 @@ async def chapter_click(client, data, chat_id):
 
         chapterFile = await db.get(ChapterFile, chapter.url)
         
-        caption = f'{chapter.name.replace("Chapter", "Ch -")} {chapter.manga.name}\n' if not str(chat_id).startswith("-100") else ""
+        caption = f'{chapter.name.replace("Chapter", "Ch -")} {chapter.manga.name}\n' if not str(chat_id).startswith("-100") else "➤ Main Channel: @manga_universe"
         if options & OutputOptions.Telegraph:
             caption += f'[Read on telegraph]({chapterFile.telegraph_url})\n' 
         caption += f'[Read on website]({chapter.get_url()})' if not str(chat_id).startswith("-100") else ""

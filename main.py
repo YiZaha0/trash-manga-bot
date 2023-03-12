dd845f5d0b0982572ecb8c125426e0ee7e88282b
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-03-13 02:06:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import sys
 import asyncio as aio
 import glob
 import os
-import uvloop
-uvloop.install()
 
 from bot import bot, manga_updater
 from models import DB
@@ -36,7 +34,7 @@ async def async_main():
     await db.connect()
 
 if __name__ == '__main__':
-    loop = aio.get_event_loop_policy().get_event_loop() or aio.get_event_loop_policy().new_event_loop()
+    loop = aio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(async_main())
     loop.create_task(manga_updater())
     bot.run()
